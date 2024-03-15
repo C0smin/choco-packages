8872056fc1868c4433561284b8d189a097f5fd97
Cosmin <13046837+C0smin@users.noreply.github.com>
2024-03-15 17:50:42 +0100

    Update SimpleX 5.5.6

--- packages/simplex/tools/chocolateyInstall.ps1
+++ packages/simplex/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     fileType       = 'msi'
-    url            = 'https://github.com/simplex-chat/simplex-chat/releases/download/v5.5.5/simplex-desktop-windows-x86_64.msi'
-    checksum       = '432D77933E545608CC5F76063C6FC148432B7440C2012BAC125B67952ACA8122'
+    url            = 'https://github.com/simplex-chat/simplex-chat/releases/download/v5.5.6/simplex-desktop-windows-x86_64.msi'
+    checksum       = '59565423D44645EB91A2C3CD590CD625972A259948060204B76221C24F0FE80A'
     checksumType   = 'sha256'
     silentArgs     = '/quiet /qn /norestart'
     validExitCodes = @(0, 1641, 3010)
