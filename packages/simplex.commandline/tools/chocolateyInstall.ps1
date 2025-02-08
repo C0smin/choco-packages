bc8b0d6c90eec2bbad318f67fe32963afec4226b
Cosmin <13046837+C0smin@users.noreply.github.com>
2025-02-08 10:05:08 +0100

    Updates simplex to 6.2.4

--- packages/simplex.commandline/tools/chocolateyInstall.ps1
+++ packages/simplex.commandline/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $fileLocation = "$toolsDir\simplex.exe"
 $webFileParameters = @{
     packageName  = $env:ChocolateyPackageName
     fileFullPath = $fileLocation
-    url          = 'https://github.com/simplex-chat/simplex-chat/releases/download/v6.2.1/simplex-chat-windows-x86-64'
-    checksum     = '87080F0B9455F0553EBAEAE1C6F12188B60819E17CFA4ACB9CC41B5C0BCBA6A2'
+    url          = 'https://github.com/simplex-chat/simplex-chat/releases/download/v6.2.4/simplex-chat-windows-x86-64'
+    checksum     = '15A8EF17B0CA88A0FDE07FA36EF63DBCD0119D9014AD2751E03AA6BC06E6FC4B'
     checksumType = 'sha256'
 }
 Get-ChocolateyWebFile @webFileParameters
