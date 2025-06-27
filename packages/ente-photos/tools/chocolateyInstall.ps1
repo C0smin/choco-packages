56a4f16492763201c0f3260311f721dec5a7f6be
Cosmin <13046837+C0smin@users.noreply.github.com>
2025-06-27 20:24:23 +0200

    Ente 1.7.13

--- packages/ente-photos/tools/chocolateyInstall.ps1
+++ packages/ente-photos/tools/chocolateyInstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     fileType       = 'exe'
-    url            = 'https://github.com/ente-io/photos-desktop/releases/download/v1.7.12/ente-1.7.12.exe'
-    checksum       = '2B38A1C97C373EF0B1072C9CD0018139E444554782325B5C5E99E3EBB13394A2'
+    url            = 'https://github.com/ente-io/photos-desktop/releases/download/v1.7.13/ente-1.7.13.exe'
+    checksum       = 'D8D4A7AFA0EA7C919C1A01E2BEFE1AF37DB8F46FA8351D8A20A4B1E6CAE732F3'
     checksumType   = 'sha256'
-    url64          = 'https://github.com/ente-io/photos-desktop/releases/download/v1.7.12/ente-1.7.12-x64.exe'
-    checksum64     = '12DDF47EB28CD543F139B1A80655E96156371DB8227B4F349ED18992C110D1EB'
+    url64          = 'https://github.com/ente-io/photos-desktop/releases/download/v1.7.13/ente-1.7.13-x64.exe'
+    checksum64     = '093177C6F5B374AA441F7FC4F03F1D693EC9EB8315DEF19721D3DEE0549BB362'
     checksumType64 = 'sha256'
     silentArgs     = '/S'
     validExitCodes = @(0, 1641, 3010)
