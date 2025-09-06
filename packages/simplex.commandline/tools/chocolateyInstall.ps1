24c79c4a2ca2febdd8ebccdc802905f7d4050a69
Cosmin <13046837+C0smin@users.noreply.github.com>
2025-09-06 19:17:47 +0200

    Simplex 6.4.4

--- packages/simplex.commandline/tools/chocolateyInstall.ps1
+++ packages/simplex.commandline/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $fileLocation = "$toolsDir\simplex.exe"
 $webFileParameters = @{
     packageName  = $env:ChocolateyPackageName
     fileFullPath = $fileLocation
-    url          = 'https://github.com/simplex-chat/simplex-chat/releases/download/v6.4.3.1/simplex-chat-windows-x86-64'
-    checksum     = 'E6F4AABCF154DF721A83AB4A4C402BF18CCE3881E5AA14FAC9C70F74E121CEC6'
+    url          = 'https://github.com/simplex-chat/simplex-chat/releases/download/v6.4.4/simplex-chat-windows-x86-64'
+    checksum     = '1310250CBFE2D5960A20CAA428A6F562DFC9EDBAE307A6852266C8B719E35AB3'
     checksumType = 'sha256'
 }
 Get-ChocolateyWebFile @webFileParameters
--- packages/simplex/tools/chocolateyInstall.ps1
+++ packages/simplex/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     fileType       = 'msi'
-    url            = 'https://github.com/simplex-chat/simplex-chat/releases/download/v6.4.3.1/simplex-desktop-windows-x86_64.msi'
-    checksum       = '78E2D7A4F41C9CE1D579DF678BFDFB4952176A4B333B3FADF0E4339473B1B994'
+    url            = 'https://github.com/simplex-chat/simplex-chat/releases/download/v6.4.4/simplex-desktop-windows-x86_64.msi'
+    checksum       = '57BA1682EC1CBB2B9499ABF2ACD4E7A0C3897DD5E36DCCE5FD217D9811CEA66C'
     checksumType   = 'sha256'
     silentArgs     = '/quiet /qn /norestart'
     validExitCodes = @(0, 1641, 3010)
