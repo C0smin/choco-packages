74c5e850da673ba1eafa344796b75ad5a3dbb83d
Cosmin <13046837+C0smin@users.noreply.github.com>
2025-09-06 19:14:30 +0200

    Initial publish of reddit-dl

---
+++ packages/reddit-dl/README.md
@@ -0,0 +1,9 @@
+# reddit-dl
+
+## Package author note
+
+**Important: This is an unofficial version, the chocolatey package is community maintained and only based on the releases made available by the SimpleX team, use the Contact Maintainers link for package related issues.**
+
+## Description
+
+A console application for downloading Reddit content.
---
+++ packages/reddit-dl/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+    packageName   = $env:ChocolateyPackageName
+    unzipLocation = $toolsDir
+    fileType      = 'ZIP'
+    url           = 'https://github.com/patrickkfkan/reddit-dl/releases/download/v1.0.0/reddit-dl-1.0.0-win32-x64.zip'
+    checksum      = '5D2DD2CB28AEB22974CD178683824AA97AA2D68C738E0E968A90B3289A4C3F22'
+    checksumType  = 'sha256'
+}
+Install-ChocolateyZipPackage @packageArgs
+
+Remove-Item -Path "$toolsDir\$(([Uri]$packageArgs['url']).Segments[-1])" -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "$env:TEMP\chocolatey\$env:ChocolateyPackageName" -Recurse -Force -ErrorAction SilentlyContinue
